c9923ae1a3d0be97bcb1bd31668af6cfd81476b3
Max Desiatov <max.desiatov@gmail.com>
2018-02-11 13:03:21 +0000

    Initial Commit

---
+++ Stardust/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Stardust
+//
+//  Created by Max Desiatov on 11/02/2018.
+//  Copyright © 2018 Max Desiatov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
